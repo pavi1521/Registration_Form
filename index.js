d6df0eb1e49e2a5bc20826f1575affcf48fb9d06
pavithra <pavithiru04@gmail.com>
2024-06-26 17:49:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+var express=require("express")
+var bodyParser=require("body-parser")
+var mongoose=require("mongoose")
+
+const app=express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+mongoose.connect('mongodb://localhost:27017/Database')
+var db=mongoose.connection
+db.on('error',()=>console.log("error in connectiong to database") )
+db.once('open',()=>console.log("Connected to database"))
+
+app.post("/sign_up",(req,res)=>{
+   var name= req.body.name
+   var age=req.body.age
+   var email=req.body.email
+   var phno=req.body.phno
+   var gender=req.body.phno
+   var password=req.body.password
+
+   var data={
+    "name" :name,
+    "age":age,
+    "email":email,
+    "phno":phno,
+    "gender":gender,
+    "password":password
+   }
+   db.collection('users').insertOne(data,(err,collection)=>{
+    if(err){
+        throw err;
+    }
+    console.log("Record Inserted Successfully")
+   })
+   return res.redirect('signup_successful.html')
+})
+app.get("/",(req,res)=>{
+    res.set({
+        "Allow-access-Allow-Origin":'*'
+    })
+    return res.redirect('index.html')
+}).listen(3000);
+console.log("Listening on port")
